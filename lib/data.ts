93cc836991ac629f844dc8c3bc6ad795fd25bd4a
slm1906 <slm1906@g.rit.edu>
2024-04-15 17:21:12 -0400

    add my info

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,9 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
+import { CgCalibrate, CgCap, CgCloud, CgLaptop, CgVolume, CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 import handwritingRobotImg from "@/public/handwritingRobot.jpg";
+import sipSageImg from "@/public/SipSage.png"
+import { LuAccessibility, LuGraduationCap } from "react-icons/lu";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
@@ -35,24 +36,63 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Experience 1",
-    location: "location",
+    title: "Northrop Grumman Mission Systems Intern",
+    location: "Baltimore, MD",
     description:
-      "A description of the things I did",
-    icon: React.createElement(FaReact),
-    date: "2019",
+      "At Northrop I helped extend an internal system for tracking the production of semiconductors with C#",
+    icon: React.createElement(CgWorkAlt),
+    date: "May 2020 - August 2020",
   },
   {
-    title: "Experience 2",
-    location: "Location 2",
+    title: "Liberty Mutual Techstart Intern",
+    location: "Portsmouth, NH",
     description:
-      "Another description of the things I did at this other place",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+      "At Liberty I worked with the cloud expansion team to build a configuration system using AWS Lambda & DocumentDB",
+    icon: React.createElement(CgCloud),
+    date: "May 2021 - August 2021",
+  }, {
+    title: "HubSpot Backend Engineering Co-op",
+    location: "Boston, MA",
+    description:
+      "At HubSpot I worked with the Access Managment group to help build systems for user settings management with Java Microservices.",
+    icon: React.createElement(CgLaptop),
+    date: "January 2022 - May 2022",
+  }, {
+    title: "Amazon Robotics Intern",
+    location: "Reading, MA",
+    description:
+      "At Amazon I worked in Kotlin to build accessibility features into human-robot interaction systems used in the Amazon Warehouses.",
+    icon: React.createElement(LuAccessibility),
+    date: "May 2022 - August 2022",
   },
+  {
+    title: "HubSpot Backend Engineering Co-op",
+    location: "Boston, MA",
+    description:
+      "I returned to HubSpot to work with the User Setup team to increase the reliability of essential HubSpot services using Netflix's Hollow Framework.",
+    icon: React.createElement(CgLaptop),
+    date: "January 2023 - August 2023",
+  },
+  {
+    title: "HubSpot SWE",
+    location: "Boston, MA",
+    description:
+      "I'm excited to rejoin HubSpot after I graduate!",
+    icon: React.createElement(LuGraduationCap),
+    date: "August 2024 onward",
+  }
 ] as const;
 
 export const projectsData = [
+  {
+    title: "SipSage",
+    description:
+      "I worked with my friend to build SipSage, a quiz application to recommend coffee & tea from female founded companies." +
+      " We won the Constellation Brands Elevating Female Founders award at WiCHacks 2024",
+    tags: ["Next.js", "3D Typescript", "Vercel"],
+    imageUrl: sipSageImg,
+    url: "http://sipsage.co"
+  },
   {
     title: "Handwriting Robot",
     description:
@@ -63,30 +103,30 @@ export const projectsData = [
 ] as const;
 
 export const skillsData = [
-  "Skills",
-  "I",
-  "Have",
-  // "TypeScript",
-  // "React",
-  // "Next.js",
+  "Java",
+  "AWS Lambda",
+  "Typescript",
+  "Netflix Hollow",
+  "AWS DocumentDb",
+  "Next.js",
   // "Node.js",
-  // "Git",
-  // "Tailwind",
+  "Git",
+  "Tailwind",
   // "Prisma",
   // "MongoDB",
   // "Redux",
   // "GraphQL",
   // "Apollo",
   // "Express",
-  // "PostgreSQL",
-  // "Python",
+  "PostgreSQL",
+  "Python",
   // "Django",
   // "Framer Motion",
 ] as const;
-export const firstName: String = "Name" as const
-export const major: String = "Major" as const
-export const year: String = "0th year" as const
-export const loveToDo = "Something" as const
-export const email = "example@gmail.com" as const
-export const linkedin = "https://www.linkedin.com" as const
-export const github = "https://www.github.com" as const
+export const firstName: String = "Sam" as const
+export const major: String = "Software Engineering" as const
+export const year: String = "5th year" as const
+export const loveToDo = "build things that help people" as const
+export const email = "slm1906@rit.edu" as const
+export const linkedin = "https://www.linkedin.com/in/samantha-mcquate" as const
+export const github = "https://www.github.com/sammcq8" as const
