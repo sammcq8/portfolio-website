8599b02dc07336110efc1f60426534f40d831cf0
slm1906 <slm1906@g.rit.edu>
2024-04-15 13:41:20 -0400

    fix email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -28,7 +28,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: {email},
+      to: email,
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
